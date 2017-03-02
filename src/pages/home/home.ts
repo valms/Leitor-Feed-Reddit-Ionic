dd79cfc8b750b47bf3b7f4668ec5eae853b5d3f3
Valmar Júnior <valmar.junior1@gmail.com>
2017-03-02 20:55:11 -0300

    Implementado o feedback do usuário e correção dos thumbnails

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,47 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, Loading } from 'ionic-angular';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  
-  public incomingFeedsReddit: Array<string>;
-  private url: string = "https://www.reddit.com/new.json"; 
-  
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  public redditResponseFeed: Array<any>;
+  private url: string = "https://www.reddit.com/new.json";
 
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+
+    this.lerFeedReddit();
+
+  }
+
+  lerFeedReddit(): void {
+    let animacaoCarregamento = this.loadingCtrl.create({
+      content: 'Carregando Conteúdo...'
+    });
+
+    animacaoCarregamento.present();
+
+    this.conveterFeedJson(animacaoCarregamento);
+
+  }
+
+  conveterFeedJson(animacaoCarregamento: Loading): void {
     this.http.get(this.url).map(res => res.json())
       .subscribe(data => {
-        this.incomingFeedsReddit = data.data.children;
-      }); 
+        this.redditResponseFeed = data.data.children;
+
+        this.redditResponseFeed.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        })
+        // Após ler o feed, dispensar a animação
+        animacaoCarregamento.dismiss();
+      });
   }
 
 }
