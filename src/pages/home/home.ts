16254a3d5c62907b8a2cee1f01a523c124cc9985
Valmar Júnior <valmar.junior1@gmail.com>
2017-03-01 23:06:57 -0300

    Feed capturado com sucesso

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  public incomingFeedsReddit: Array<string>;
+  private url: string = "https://www.reddit.com/new.json"; 
+  
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.http.get(this.url).map(res => res.json())
+      .subscribe(data => {
+        this.incomingFeedsReddit = data.data.children;
+      }); 
   }
 
 }
